798bfbff0b7442b47789a75b3f3160bc0de7b7d8
Hruday <Hruday@LAPTOP-A3PCRR3L>
2023-07-11 22:32:56 +0530

    Stack

--- MavenLearning/src/test/java/Selenium_Methods/A.java
+++ MavenLearning/src/test/java/Selenium_Methods/A.java
@@ -1,14 +1,20 @@
 package Selenium_Methods;
 
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-
-
-
+@Listeners(Selenium_Methods.Listeners.class)
 public class A  {
 	
 	@Test
 	public void passTest() {
 		System.out.println("Passed");
+	}
+	
+	@Test
+	public void failTest() {
+		Assert.fail();
+		System.out.println("Failed");
 	}
 }
